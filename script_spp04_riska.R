7e6cb6ec8ad054604ff5ee83ebea8b6dfc42fffa
Shohibul Ulum <84631934+shulum30994@users.noreply.github.com>
2023-06-25 11:39:08 +0700

    Update script_spp04_riska.R
    Script untuk melakukan regresi logistik dari data SPP04. Model untuk jurnal yang dikerjakan oleh sdr Riska

--- script_spp04_riska.R
+++ script_spp04_riska.R
@@ -4,10 +4,12 @@ library(DescTools)
 library(stargazer)
 library(foreign)
 
+spp104 <- read.dbf("/home/shohibul/KERJA/DATA/Buku Analisis Lahan/Referensi/spp1.dbf")
 spp204 <- read.dbf("/home/shohibul/KERJA/DATA/Buku Analisis Lahan/Referensi/spp2.dbf")
 demogspp <- read.dbf("/home/shohibul/KERJA/DATA/Buku Analisis Lahan/Referensi/demogspp.dbf")
 
 mentahan <- spp204 %>% select(IDENTITAS,PROP,B16RF,B17R5) %>% na.omit()
+transfer_lahan <- spp104 %>% select(IDENTITAS,B5R3A)
 
 # hitung kepala jumlah anggota keluarga
 keluarga<-demogspp %>%
@@ -16,14 +18,31 @@ keluarga<-demogspp %>%
   group_by(IDENTITAS) %>%
   count(IDENTITAS)
 
-pendidikan<-demogspp %>%
+demo<-demogspp %>%
   filter(B3K3==1) %>%
-  select(IDENTITAS,B3K6)
+  select(IDENTITAS,B3K4,B3K5,B3K6) %>%
+  na.omit()
   
 # gabungkan data
+mentahan <- merge(mentahan,transfer_lahan,by="IDENTITAS",all = TRUE)
 mentahan <- merge(mentahan,keluarga,by="IDENTITAS",all = TRUE)
 mentahan <- mentahan%>%rename(AGG_KEL=n)
-mentahan <- merge(mentahan,pendidikan,by="IDENTITAS",all = TRUE)
+mentahan <- merge(mentahan,demo,by="IDENTITAS",all = TRUE)
 mentahan[is.na(mentahan)]<-0
+siap <- mentahan %>% na.omit()
 
 # variabel baru dari variabel yang telah ada
+siap<-siap %>%
+  mutate(TRANSFER=if_else(B5R3A==1,1,0),GENDER=if_else(B3K4==1,1,0),DSD = if_else(B3K6==2,1,0),DSMP=if_else(B3K6==3,1,0),DSMA=if_else(B3K6==4,1,0),DPT=if_else(B3K6 %in% c(5,6,7),1,0),DKREDIT=if_else(B17R5==2,1,0))
+
+# model
+REG_NUl <- glm(as.factor(TRANSFER) ~ 1,family = "binomial",data = siap)
+MODEL <- glm(as.factor(TRANSFER) ~ B3K5+as.factor(GENDER)+as.factor(DSD)+as.factor(DSMP)+as.factor(DSMA)+as.factor(DPT)+AGG_KEL+B16RF+as.factor(DKREDIT),family = "binomial",data = siap)
+
+# hasil estimasi
+stargazer(MODEL,type = "text")
+
+# fitting model
+anova(REG_NUl,MODEL,test = "LRT")
+hoslem.test(siap$TRANSFER,fitted(MODEL),g=10)
+PseudoR2(MODEL,which = c("McFadden","McFaddenAdj","Negelkerke","Coxsnell"))
