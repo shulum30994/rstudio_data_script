96313be7955c1975d7be1582b8f4d1b77801f57c
Shohibul Ulum <84631934+shulum30994@users.noreply.github.com>
2023-07-27 09:16:34 +0700

    tematik_renting_in_out.R
    Script R untuk membuat peta tematik dari data SPP 2004 & SPP 2014.

--- tematik_renting_in_out.R
+++ tematik_renting_in_out.R
@@ -1,50 +1,87 @@
+library(ragg)
 library(tmap)
 library(sf)
 library(tidyverse)
 library(RColorBrewer)
 library(hrbrthemes)
 
-id_map <- read_sf("/home/shohibul/Pemetaan/gadm41_IDN_shp/gadm41_IDN_1.shp")
+id_map <- read_sf("C:/Users/riset/Documents/1. PERFORMA CENDEKIA/2023/idn_GADM/gadm41_IDN_1.shp")
 siap04 <- read.csv("/home/shohibul/R Wokspace/spp_riska/siap04.csv")
 siap04 <- siap04 %>% mutate(TRANSFER_RATIO=(RENTIN+RENTOUT)/B5R1DK5)
 siap14 <- read.csv("/home/shohibul/R Wokspace/spp_riska/siap14.csv")
 
 siap04 %>%
   group_by(PROP) %>%
-  summarise(across(c(RENTIN),.fns=sum)) %>%
-  rename(VALUE=RENTIN) %>%
-  add_column(KATEGORI="RENTING IN")
+  summarise(across(c(IN_RATIO),.fns=mean)) %>%
+  rename(VALUE=IN_RATIO) %>%
+  add_column(KATEGORI="RENTING IN RATIO",TAHUN="2004")
 
 siap04 %>%
   group_by(PROP) %>%
-  summarise(across(c(RENTOUT),.fns=sum))%>%
-  rename(VALUE=RENTOUT) %>%
-  add_column(KATEGORI="RENTING OUT")
+  summarise(across(c(OUT_RATIO),.fns=mean))%>%
+  rename(VALUE=OUT_RATIO) %>%
+  add_column(KATEGORI="RENTING OUT RATIO",TAHUN="2004")
 
 siap04 %>%
   group_by(PROP) %>%
-  summarise(across(c(B5R1DK5),.fns=sum)) %>%
-  rename(VALUE=B5R1DK5) %>%
-  add_column(KATEGORI="ACREAGE")
+  summarise(across(c(TRANSFER_RATIO),.fns=mean))%>%
+  rename(VALUE=TRANSFER_RATIO) %>%
+  add_column(KATEGORI="LAND TRANSFER RATIO",TAHUN="2004")
 
-trans_2004<-bind_rows(
+siap14 %>%
+  group_by(PROP_x) %>%
+  summarise(across(c(IN_RATIO),.fns=mean)) %>%
+  rename(VALUE=IN_RATIO,PROP=PROP_x) %>%
+  add_column(KATEGORI="RENTING IN RATIO",TAHUN="2014")
+
+siap14 %>%
+  group_by(PROP_x) %>%
+  summarise(across(c(OUT_RATIO),.fns=mean))%>%
+  rename(VALUE=OUT_RATIO,PROP=PROP_x) %>%
+  add_column(KATEGORI="RENTING OUT RATIO",TAHUN="2014")
+
+siap14 %>%
+  group_by(PROP_x) %>%
+  summarise(across(c(TRANSFER_RATIO),.fns=mean))%>%
+  rename(VALUE=TRANSFER_RATIO,PROP=PROP_x) %>%
+  add_column(KATEGORI="LAND TRANSFER RATIO",TAHUN="2014")
+
+trans_all<-bind_rows(
   siap04 %>%
     group_by(PROP) %>%
-    summarise(across(c(RENTIN),.fns=sum)) %>%
-    rename(VALUE=RENTIN) %>%
-    add_column(KATEGORI="RENTING IN")%>%
+    summarise(across(c(IN_RATIO),.fns=mean)) %>%
+    rename(VALUE=IN_RATIO) %>%
+    add_column(KATEGORI="RENTING IN RATIO",TAHUN="2004")%>%
     na.omit(),
   siap04 %>%
     group_by(PROP) %>%
-    summarise(across(c(RENTOUT),.fns=sum))%>%
-    rename(VALUE=RENTOUT)%>%
-    add_column(KATEGORI="RENTING OUT")%>%
+    summarise(across(c(OUT_RATIO),.fns=mean))%>%
+    rename(VALUE=OUT_RATIO) %>%
+    add_column(KATEGORI="RENTING OUT RATIO",TAHUN="2004")%>%
     na.omit(),
   siap04 %>%
     group_by(PROP) %>%
-    summarise(across(c(B5R1DK5),.fns=sum)) %>%
-    rename(VALUE=B5R1DK5)%>%
-    add_column(KATEGORI="ACREAGE")%>%
+    summarise(across(c(TRANSFER_RATIO),.fns=mean))%>%
+    rename(VALUE=TRANSFER_RATIO) %>%
+    add_column(KATEGORI="LAND TRANSFER RATIO",TAHUN="2004")%>%
+    na.omit(),
+  siap14 %>%
+    group_by(PROP_x) %>%
+    summarise(across(c(IN_RATIO),.fns=mean)) %>%
+    rename(VALUE=IN_RATIO,PROP=PROP_x) %>%
+    add_column(KATEGORI="RENTING IN RATIO",TAHUN="2014")%>%
+    na.omit(),
+  siap14 %>%
+    group_by(PROP_x) %>%
+    summarise(across(c(OUT_RATIO),.fns=mean))%>%
+    rename(VALUE=OUT_RATIO,PROP=PROP_x) %>%
+    add_column(KATEGORI="RENTING OUT RATIO",TAHUN="2014")%>%
+    na.omit(),
+  siap14 %>%
+    group_by(PROP_x) %>%
+    summarise(across(c(TRANSFER_RATIO),.fns=mean))%>%
+    rename(VALUE=TRANSFER_RATIO,PROP=PROP_x) %>%
+    add_column(KATEGORI="LAND TRANSFER RATIO",TAHUN="2014")%>%
     na.omit()
 )
 
@@ -65,6 +102,8 @@ id_map %>%
 
 #### Plot The Map ####
 map04 <- left_join(id_map,trans_2004,by=c("CC_1"="PROP"))
+thm_map <- left_join(id_map,trans_all,by=c("CC_1"="PROP"))
+
 tm_shape(id_map)+
   tm_borders(alpha = 0.1)+
   tm_shape(map04%>%na.omit)+
@@ -74,7 +113,11 @@ tm_shape(id_map)+
 
 ggplot()+
   geom_sf(data=id_map)+
-  geom_sf(data=map04%>%na.omit,aes(fill=VALUE/10000))+
+  geom_sf(data=thm_map%>%na.omit,aes(fill=VALUE))+
   scale_fill_distiller(palette = "Blues",direction = 1)+
-  facet_wrap(~KATEGORI,nrow = 2,ncol = 2)+
-  theme_linedraw()
+  labs(fill="Ratio :")+
+  facet_wrap(KATEGORI~TAHUN,nrow = 3,ncol = 2)+
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())
