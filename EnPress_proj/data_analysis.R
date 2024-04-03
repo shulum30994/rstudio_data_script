c5f52072224a8b9b98f2805ff789e0eb5ce9fd23
Shohibul Ulum <84631934+shulum30994@users.noreply.github.com>
2024-04-03 22:22:45 +0700

    Update data_analysis.R

--- EnPress_proj/data_analysis.R
+++ EnPress_proj/data_analysis.R
@@ -79,12 +79,76 @@ data14<-data14 %>%
       PROP=="91" ~ 1,
       PROP=="94" ~ 1,
       TRUE ~ 0,
-    )
+    ),
+    S_NS=if_else(R503B_I1!=0,"S_NS",""),
+    S_NP=if_else(R503B_I2!=0,"S_NP",""),
+    NS_S=if_else(R503B_II1!=0,"NS_S",""),
+    NS_NP=if_else(R503B_II2!=0,"NS_NP",""),
+    NP_S=if_else(R503B_III1!=0,"NP_S",""),
+    NP_NS=if_else(R503B_III2!=0,"NP_NS",""),
+    KOMBINASI = gsub(",{2,}", ",", 
+                     gsub("^,+|,+$", "", paste(S_NS,S_NP,NS_S,NS_NP,NP_S,NP_NS,sep = ","))),
+    KONVERSI_LAHAN = case_when(
+	  KOMBINASI == 'NP_NS'~1,
+    KOMBINASI == 'S_NS'~2,
+    KOMBINASI == 'NS_NP'~3,
+    KOMBINASI == 'NS_S'~1,
+    KOMBINASI == 'S_NP'~2,
+    KOMBINASI == 'NP_S'~1,
+    KOMBINASI == 'NS_NP,NP_NS'~1,
+    KOMBINASI == 'S_NS,S_NP'~1,
+    KOMBINASI == 'S_NS,NS_S'~1,
+    KOMBINASI == 'S_NS,NP_NS'~1,
+    KOMBINASI == 'NP_S,NP_NS'~1,
+    KOMBINASI == 'S_NS,NS_NP'~1,
+    KOMBINASI == 'NS_S,NP_NS'~1,
+    KOMBINASI == 'S_NP,NS_NP'~2,
+    KOMBINASI == 'S_NP,NP_NS'~1,
+    KOMBINASI == 'NS_S,NS_NP'~2,
+    KOMBINASI == 'S_NP,NS_S'~2,
+    KOMBINASI == 'NS_S,NP_S'~1,
+    KOMBINASI == 'S_NS,NP_S'~1,
+    KOMBINASI == 'S_NS,S_NP,NP_NS'~1,
+    KOMBINASI == 'S_NS,S_NP,NS_S'~1,
+    KOMBINASI == 'S_NP,NP_S'~1,
+    KOMBINASI == 'S_NS,NS_NP,NP_NS'~3,
+    KOMBINASI == 'S_NS,NS_S,NP_NS'~1,
+    KOMBINASI == 'S_NS,S_NP,NS_NP'~2,
+    KOMBINASI == 'NS_NP,NP_S,NP_NS'~1,
+    KOMBINASI == 'NS_S,NS_NP,NP_NS'~1,
+    KOMBINASI == 'S_NP,NS_NP,NP_NS'~2,
+    KOMBINASI == 'S_NS,NP_S,NP_NS'~1,
+    KOMBINASI == 'S_NS,S_NP,NS_NP,NP_NS'~2,
+    KOMBINASI == 'S_NS,S_NP,NS_S,NS_NP'~1,
+    KOMBINASI == 'NS_NP,NP_S'~3,
+    KOMBINASI == 'S_NP,NP_S,NP_NS'~1,
+    KOMBINASI == 'S_NP,NS_NP,NP_S'~3,
+    KOMBINASI == 'S_NP,NS_S,NP_S'~3,
+    KOMBINASI == 'S_NS,NS_S,NP_S'~3,
+    KOMBINASI == 'S_NS,NS_S,NS_NP'~1,
+    KOMBINASI == 'S_NS,S_NP,NS_S,NP_NS'~1,
+    KOMBINASI == 'S_NS,S_NP,NS_S,NS_NP,NP_S,NP_NS'~1,
+    TRUE ~ 0
+	),
+	RASIO_KONVERSI = (((R503B_I1+R503B_I2+R503B_II1+R503B_II2+R503B_III1+R503B_III2)/R501D_K6)*100)
   )
+data14 %>% group_by(KOMBINASI) %>% count(KOMBINASI) %>% arrange(desc(n)) %>% print(n=40)
+
+data14 %>% group_by(URBAN_STAT) %>% count(as.factor(KONVERSI_LAHAN)) # status urban
+
+data14 %>% group_by(B3_K7) %>% count(as.factor(KONVERSI_LAHAN)) # spendidikan
+
+data14 %>% group_by(SERTIPIKAT) %>% count(as.factor(KONVERSI_LAHAN)) # sertifikat tanah
+
+data14 %>% group_by(KOPERASI) %>% count(as.factor(KONVERSI_LAHAN)) # koperasi
+
+data14 %>% group_by(POKTAN) %>% count(as.factor(KONVERSI_LAHAN)) # poktan
+
+data14 %>% group_by(KREDIT) %>% count(as.factor(KONVERSI_LAHAN)) # poktan
 
 MODEL1 <- glm(as.factor(KONVERSI_LAHAN)~as.factor(AKSES_PASAR)+R501D_K6+R501E1_K6+B3_K5+as.factor(B3_K7)+jumlah_anggota_kel+B16F_K2+B16A_K2+as.factor(SERTIPIKAT)+as.factor(KOPERASI)+as.factor(POKTAN)+as.factor(KREDIT)+RASIO+as.factor(ISLAND)+as.factor(URBAN),data14 %>% filter(PROP!="31") %>% na.omit(),family = "binomial")
 
-MULTINOM1 <- multinom(as.factor(JENIS_KONVERSI)~as.factor(AKSES_PASAR)+R501D_K6+R501E1_K6+B3_K5+as.factor(B3_K7)+jumlah_anggota_kel+B16F_K2+B16A_K2+as.factor(SERTIPIKAT)+as.factor(KOPERASI)+as.factor(POKTAN)+as.factor(KREDIT)+Total_Panjang+as.factor(DSUMA)+as.factor(DKALI)+as.factor(DBALI)+as.factor(DSULA)+as.factor(DPAPU)+as.factor(DPAPU)+as.factor(URBAN),data=data14 %>% filter(PROP!="31") %>% na.omit())
+MULTINOM1 <- multinom(as.factor(KONVERSI_LAHAN)~as.factor(AKSES_PASAR)+R501D_K6+R501E1_K6+B3_K5+as.factor(B3_K7)+jumlah_anggota_kel+B16F_K2+B16A_K2+as.factor(SERTIPIKAT)+as.factor(KOPERASI)+as.factor(POKTAN)+as.factor(KREDIT)+Total_Panjang+as.factor(DSUMA)+as.factor(DKALI)+as.factor(DBALI)+as.factor(DSULA)+as.factor(DPAPU)+as.factor(DPAPU)+as.factor(URBAN),data=data14 %>% na.omit())
 
 stargazer(MULTINOM1,
           #poktan.remit.urban,
