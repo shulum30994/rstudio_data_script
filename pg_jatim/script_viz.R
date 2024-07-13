c01c72f43e317375e617b223c11d99800b052ae6
Shohibul Ulum <84631934+shulum30994@users.noreply.github.com>
2024-07-13 19:46:15 +0700

    Create script_viz.R
    Script R untuk visualisasi lokasi PG aktif di seluruh Jawa.

---
+++ pg_jatim/script_viz.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(sf)
+
+ind_kec <- read_sf("G:/SPATIAL/idn_GADM/gadm41_IDN_3.shp") %>% st_transform(4326)
+
+ind_jatim <- ind_kec %>%
+  filter(NAME_1=="Jawa Timur"|NAME_1=="Jawa Tengah"|NAME_2=="Cirebon")
+
+ko_pg <- koordinat %>%
+  st_as_sf(coords = c("LONG","LAT"),crs=4326)
+
+wilker <- st_buffer(subset(ko_pg),10000)
+
+ind_jatim$terdekat <- ifelse(sf::st_intersects(ind_jatim,
+                                               wilker,
+                                               sparse = F),
+                             "Yes", 
+                             "No")
+ggplot()+
+  geom_sf(data=ind_jatim,aes(size=0.2,fill=terdekat))+
+  geom_sf(data=ko_pg,aes(color="black"))+
+  geom_sf(data = wilker,fill=alpha("red",0.2),color="red")+
+  theme_minimal()+
+  theme(legend.position = "none")
