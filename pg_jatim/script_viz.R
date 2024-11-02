a80bb0d29f71dacb6f0990e7d355de20946b3f8c
Shohibul Ulum <84631934+shulum30994@users.noreply.github.com>
2024-11-02 18:27:48 +0700

    Update script_viz.R

--- pg_jatim/script_viz.R
+++ pg_jatim/script_viz.R
@@ -1,10 +1,14 @@
 library(tidyverse)
 library(sf)
+library(tmap)
 
 ind_kec <- read_sf("G:/SPATIAL/idn_GADM/gadm41_IDN_3.shp") %>% st_transform(4326)
 
 ind_jatim <- ind_kec %>%
   filter(NAME_1=="Jawa Timur"|NAME_1=="Jawa Tengah"|NAME_2=="Cirebon")
+
+ind_jawa_prop <- ind_prop %>%
+  filter(NAME_1=="Jawa Timur"|NAME_1=="Jawa Tengah"|NAME_1=="Jawa Barat"|NAME_1=="Yogyakarta"|NAME_1=="Banten")
 
 ko_pg <- koordinat %>%
   st_as_sf(coords = c("LONG","LAT"),crs=4326)
@@ -22,3 +26,9 @@ ggplot()+
   geom_sf(data = wilker,fill=alpha("red",0.2),color="red")+
   theme_minimal()+
   theme(legend.position = "none")
+
+tm_shape(ind_jawa_prop)+
+  tm_borders(alpha = 0.7)+
+  tm_shape(ko_pg)+
+  tm_symbols(size = "TCD",
+             palette = "Reds")
