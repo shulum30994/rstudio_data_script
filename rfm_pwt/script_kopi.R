1fbc50e93c36719a24582979482eddb104f9fc9d
Shohibul Ulum <84631934+shulum30994@users.noreply.github.com>
2025-06-10 21:56:07 +0700

    Update script_kopi.R

--- rfm_pwt/script_kopi.R
+++ rfm_pwt/script_kopi.R
@@ -29,12 +29,24 @@ kopi_901212000 %>% arrange(desc(YEAR_DATE)) %>% head(5)
 
 # perform RFM analysis
 rfm_kopi_901212000 <- rfm_table_order(
-  data = kopi_901212000 %>% na.omit(),
-  customer_id = DESTCTRY,
-  revenue = revenue,
+  data=kopi_901212000,
+  customer_id = DESTCTRY_L,
+  revenue = REVENUE,
   order_date = YEAR_DATE,
   analysis_date = as.Date("2016-12-02")
 )
+
+# convert into dataframe
+rfm_kopi_901212000_df <- as.data.frame(rfm_kopi_901212000$rfm)
+
+# calculate weighted RFM score
+rfm_kopi_901212000_df<-rfm_kopi_901212000_df %>% mutate(W.SCORE=5*recency_score+2*frequency_score+monetary_score)
+
+# rfm score and weighted score
+rfm_kopi_901212000_df %>%
+  select(customer_id,rfm_score,W.SCORE) %>%
+  group_by(customer_id) %>%
+  summarise(across(c("rfm_score","W.SCORE"),.fns=mean))
 
 # heatmap
 rfm_plot_heatmap(rfm_kopi_901212000)
@@ -45,71 +57,25 @@ rfm_plot_bar_chart(rfm_kopi_901212000)
 # scatter plot
 rfm_plot_segment_scatter(rfm_kopi_901212000)
 
-# convert into dataframe
-rfm_kopi_901212000_df <- as.data.frame(rfm_kopi_901212000$rfm)
+# segment names
+segment_names <- c("Champions", "Potential Loyalist", "Loyal Customers",
+                   "Promising", "New Customers", "Can't Lose Them",
+                   "At Risk", "Need Attention", "About To Sleep", "Lost")
 
-rfm_kopi_901212000_df %>%
- # group_by(recency_score) %>%
-  summarise(across(c('recency_days',
-                     'transaction_count',
-                     'amount'),mean))
+# segment intervals
+recency_lower <- c(5, 3, 2, 3, 4, 1, 1, 1, 2, 1)
+recency_upper <- c(5, 5, 4, 4, 5, 2, 2, 3, 3, 1)
+frequency_lower <- c(5, 3, 2, 1, 1, 3, 2, 3, 1, 1)
+frequency_upper <- c(5, 5, 4, 3, 3, 4, 5, 5, 3, 5)
+monetary_lower <- c(5, 2, 2, 3, 1, 4, 4, 3, 1, 1)
+monetary_upper <- c(5, 5, 4, 5, 5, 5, 5, 5, 4, 5)
 
-rfm_kopi_901212000_df %>%
-  group_by(recency_score) %>%
-  summarise(Recency=count(as.factor(recen)))
-
-dist_901212000<-cbind(rfm_kopi_901212000_df %>%
-        group_by(recency_score) %>%
-        count(recency_score),
-      rfm_kopi_901212000_df %>%
-        group_by(frequency_score) %>%
-        count(frequency_score),
-      rfm_kopi_901212000_df %>%
-        group_by(monetary_score) %>%
-        count(monetary_score)) %>%
-  select(recency_score,n...2,n...4,n...6) %>%
-  rename(SKOR=recency_score,
-         Recency=n...2,
-         Frequency=n...4,
-         Monetary=n...6)
-
-dist_901212000<-dist_901212000 %>%
-  group_by(SKOR) %>%
-  mutate(sub_total=sum(c(Recency,Frequency,Monetary)),
-         Recency_perc=round((Recency/sub_total)*100,1),
-         Frequency_perc=round((Frequency/sub_total)*100,1),
-         Monetary_perc=round((Monetary/sub_total)*100,1))
-
-# visualisasi distribusi RFM berdasarkan negara
-rfm_kopi_901212000_df %>%
-  select(customer_id,recency_score,frequency_score,monetary_score)%>%
-  mutate(sum_RFM=recency_score+frequency_score+monetary_score,
-         Recency=(recency_score/sum_RFM)*100,
-         Frequency=(frequency_score/sum_RFM)*100,
-         Monetary=(monetary_score/sum_RFM)*100) %>%
-  #stack(c("Recency","Frequency","Monetary"))
-  pivot_longer(cols = c("Recency","Frequency","Monetary"),
-               names_to = "kategori",
-               values_to = "nilai") %>%
-  mutate(new_cat=factor(kategori, levels=
-                          c("Monetary",
-                            "Frequency",
-                            "Recency"), ordered = TRUE)) %>%
-  ggplot()+
-  aes(x=customer_id,y=nilai,fill = new_cat)+
-  geom_bar(stat = "identity")+
-  scale_fill_manual(values=c("Recency"="#084C61","Frequency"="#E3B505","Monetary"="#DB504A"))+
-  labs(fill="Keterangan",
-       title = "Proporsi skor RFM berdasarkan Negara",
-       y="Proporsi Skor",
-       x="Negara")+
-  theme(axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        legend.position = "bottom")+
-  coord_flip()
-
-# calculate weighted RFM score
-rfm_kopi_901212000_df<-rfm_kopi_901212000_df %>% mutate(W.SCORE=5*recency_score+2*frequency_score+monetary_score)
+# generate segments
+segments <- rfm_segment(rfm_kopi_901212000, segment_names, recency_lower,
+                        recency_upper, frequency_lower, frequency_upper, monetary_lower,
+                        monetary_upper)
+# rfm report
+rfm_create_report(rfm_kopi_901212000,  segments, FALSE, "Customer Segmentation Report")
 
 # save into .csv format
 write.csv(rfm_kopi_901212000_df,"rfm_result1.csv")
