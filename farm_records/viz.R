247b61ffa9b8ca6a6c613d0dc6d689762249d28f
Shohibul Ulum <84631934+shulum30994@users.noreply.github.com>
2025-08-02 16:37:02 +0700

    Create viz.R
    Skrip visualisasi data farm records

---
+++ farm_records/viz.R
@@ -0,0 +1,95 @@
+library(googlesheets4)
+library(tidyverse)
+library(directlabels)
+library(ggrepel)
+
+raw <- read_sheet("https://docs.google.com/spreadsheets/d/15j-82tISFJKrGKk5sdlE3-S6ge0geWt_xog1lEY7qgE/edit?gid=1754970135#gid=1754970135", sheet = "FARM_WORKERS")
+
+# Fee each activities
+raw %>%
+  select(Date,
+         Commodities,
+         Start_dttm,
+         End_dttm,
+         Activity,
+         `Fee (Rp)`) %>%
+  filter(Commodities=="Tobacco") %>%
+  group_by(Date,Activity) %>%
+  summarise(across(c("Fee (Rp)"),sum)) %>%
+  ggplot()+
+  aes(x=Date,y=`Fee (Rp)`)+
+  geom_line()
+
+# Working hours viz
+raw %>%
+  filter(Commodities=="Maize") %>%
+  select(Date,
+         Commodities,
+         Start_dttm,
+         End_dttm,
+         Activity,
+         `Fee (Rp)`) %>%
+  group_by(Date, Activity) %>%
+  mutate(Working_hours=difftime(End_dttm, Start_dttm, units = "hours")) %>%
+  summarise(across(c("Working_hours"),mean)) %>%
+  ggplot()+
+  aes(x=Date,y=Working_hours,fill = Activity)+
+  geom_col()
+
+red_pepper<-raw %>%
+  filter(Commodities=="Red Pepper") %>%
+  select(Date,
+         Commodities,
+         Activity,
+         `Fee (Rp)`) %>%
+  mutate(relative_cumsum=cumsum(`Fee (Rp)`)) %>%
+  group_by(Date)
+
+tobacco %>%
+  group_by(Activity) %>%
+  summarise(across(c('Fee (Rp)'),sum))
+
+tobacco<-raw %>%
+  filter(Commodities=="Tobacco") %>%
+  select(Date,
+         Commodities,
+         Start_dttm,
+         End_dttm,
+         Activity,
+         `Fee (Rp)`) %>%
+  mutate(relative_cumsum=cumsum(`Fee (Rp)`))
+
+ggplot()+
+  geom_line(data=red_pepper %>%
+              group_by(Date) %>%
+              summarise(across(c('relative_cumsum'),sum)),
+            aes(
+              x=Date,
+              y=relative_cumsum
+            ))
+
+raw %>%
+  group_by(Commodities) %>%
+  filter(Commodities=="Tobacco"|Commodities=="Red Pepper") %>%
+  select(Date,
+         Commodities,
+         Start_dttm,
+         End_dttm,
+         Activity,
+         `Fee (Rp)`) %>%
+  mutate(relative_cumsum=cumsum(`Fee (Rp)`))%>%
+ggplot()+
+  aes(x=Date,y=relative_cumsum,color=Commodities)+
+  #geom_rect(aes(ymin = 0,
+   #             ymax = Inf,
+    #            xmin = Start_dttm,
+     #           xmax = End_dttm))+
+  geom_line()+
+  #geom_label_repel(aes(label=Commodities),
+                   #nudge_x = 1,
+                  # na.rm = TRUE)+
+  theme(legend.position = "bottom")
+
+raw %>% 
+  group_by(Commodities) %>%
+  filter(Date==max(Date)|Commodities=="Tobacco"|Commodities=="Red Pepper")
