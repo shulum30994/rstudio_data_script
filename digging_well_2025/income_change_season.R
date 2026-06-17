75d22f6acaab6a51541063708c274e6dfedf4ec5
Shohibul Ulum <84631934+shulum30994@users.noreply.github.com>
2026-06-17 14:27:17 +0700

    Create income_change_season.R
    Visualize the income change among MT

---
+++ digging_well_2025/income_change_season.R
@@ -0,0 +1,33 @@
+library(googlesheets4)
+library(tidyverse)
+
+ori <- read_sheet('https://docs.google.com/spreadsheets/d/1LiBvcJulFiZJ1l9sqIC489cgQfqeetGHdG1w78tiMEw/edit?gid=0#gid=0')
+
+data<-ori %>%
+  select(parent_id,
+  crop_pattern,
+area_MT1,
+area_MT2,
+area_MT3,
+MT1_revenue,
+MT2_revenue,
+MT3_revenue,
+MT1_cost,
+MT2_cost,
+MT3_cost) %>%
+  separate(crop_pattern, into=c('MT1_comm', 'MT2_comm', 'MT3_comm'), sep = '-') %>%
+  mutate(MT1_income=MT1_revenue-MT1_cost,
+  MT2_income=MT2_revenue-MT1_cost,
+MT3_income=MT3_revenue-MT3_cost) %>%
+  mutate(MT1_income_acre=MT1_income/area_MT1,
+  MT2_income_acre=MT2_income/area_MT2,
+MT3_income_acre=MT3_income/area_MT3) %>%
+mutate(delta1=MT2_income_acre-MT1_income_acre,
+delta2=MT3_income_acre-MT2_income_acre) %>%
+  na.omit
+
+data %>%
+  filter(MT1_comm %in% c('Padi', 'Jagung') & MT2_comm %in% c('Bawang Merah','Tembakau','Cabai Merah'), delta1 > 0) %>%
+  ggplot+
+  aes(x=as.factor(parent_id), y=delta1, fill=MT2_comm)+
+  geom_col()
